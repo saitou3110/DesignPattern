0c948f8b12b34d94b56157299b82e3407f7ea002
saitou3110 <sora.mag.mag@gmail.com>
2021-01-09 17:03:37 +0900

    add method

--- chapter3/template_method.py
+++ chapter3/template_method.py
@@ -5,24 +5,36 @@ template method
 スーパークラスで処理の枠組みを定めて、サブクラスで具体的な内容を決める
 '''
 
-class AbstractDisplay(metaclass=ABCMeta):
+class AbstractDisplay(metaclass=ABCMeta):# 抽象クラス
     @abstractmethod
-    def open(self):
+    def open(self):# サブクラスに実装を任せるメソッド1
         pass
 
     @abstractmethod
-    def print(self):
+    def print(self):# サブクラスに実装を任せるメソッド2
         pass
 
     @abstractmethod
-    def close(self):
+    def close(self):# サブクラスに実装を任せるメソッド3
         pass
 
-    def display(self):
-        self.open()
+    def display(self):# この抽象クラスで実装しているメソッド
+        self.open()# まずopenして
 
-        for _ in range(5):
+        for _ in range(5):# 5回printする
             self.print()
 
-        self.close()
-        
+        self.close()# 最後にclose
+
+class CharDisplay(AbstractDisplay):
+    def __init__(self, ch):
+        self.ch = ch
+
+    def open(self):
+        print('<<')
+
+    def print(self):
+        print(self.ch)
+
+    def close(self):
+        print('>>')
